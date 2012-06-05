3a78c641b6ef5e9cfb960cca040b626507a0e901
paxchristos <nielson.peter@gmail.com>
2012-06-05 09:57:23 -0500

    Moved build.prop changes to device instead of common

--- device_zeusc.mk
+++ device_zeusc.mk
@@ -44,7 +44,7 @@ PRODUCT_LOCALES += hdpi
 -include device/semc/msm7x30-common/prebuilt/resources-hdpi.mk
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=240 \
+    ro.opengles.version=131072 \
     persist.rild.nitz_plmn= \
     persist.rild.nitz_long_ons_0= \
     persist.rild.nitz_long_ons_1= \
@@ -54,6 +54,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.rild.nitz_short_ons_1= \
     persist.rild.nitz_short_ons_2= \
     persist.rild.nitz_short_ons_3= \
+    rild.libpath=/system/lib/libril-qc-1.so \
+    rild.libargs=-d/dev/smd0 \
+    ro.ril.hsxpa=1 \
+    ro.ril.gprsclass=10 \
+    ro.ril.def.agps.mode=2 \
+    ro.ril.def.agps.feature=1 \
+    ro.telephony.default_network=4 \
+    ro.telephony.call_ring.multiple=false \
+    ro.telephony.ril_class=SemcRIL \
+    ro.ril.disable.power.collapse=0 \
+    ro.ril.fast.dormancy.timeout=3 \
+    ro.ril.enable.sbm.feature=1 \
+    pm.sleep_mode=1 \
     ro.telephony.ril.v3=datacall \
     ro.cdma.homesystem=64,65,76,77,78,79,80,81,82,83 \
     ro.cdma.home.operator.numeric=310004 \
@@ -62,4 +75,25 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.telephony.support_ipv6=true \
     persist.telephony.support_ipv4=true \
     ro.ril.vzw.feature=1 \
-    ro.ril.wp.feature=1
+    ro.ril.wp.feature=1 \
+    wifi.supplicant_scan_interval=15 \
+    keyguard.no_require_sim=true \
+    ro.com.google.locationfeatures=1 \
+    ro.product.locale.language=en \
+    ro.product.locale.region=US \
+    persist.ro.ril.sms_sync_sending=1 \
+    ro.use_data_netmgrd=true \
+    debug.sf.hw=1 \
+    debug.composition.type=dyn \
+    ro.sf.compbypass.enable=1 \
+    dev.pm.dyn_samplingrate=1 \
+    com.qc.hardware=true \
+    com.qc.hdmi_out=false \
+    BUILD_UTC_DATE=0 \
+    hwui.render_dirty_regions=false \
+    hwui.disable_vsync=true \
+    persist.sys.usb.config=mass_storage
+    ro.sf.lcd_density=240 \
+
+    
+    
